3589efd3a6477be4a847d5a3310a3dc2c9528a22
Dustin Huang <dustinhuang21@gmail.com>
2019-07-12 01:07:16 -0500

    react added

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1>Welcome to Fantasy Land</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
